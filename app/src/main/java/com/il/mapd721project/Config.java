3ca6236c661d888805ba3e28caf4014e00525847
sakash07 <sharma.akash221@gmail.com>
2022-04-19 20:18:18 -0600

    PAyPAl Billing API Integration

---
+++ app/src/main/java/com/il/mapd721project/Config.java
@@ -0,0 +1,6 @@
+package com.il.mapd721project;
+
+public class Config {
+
+    public static final String PAYPAY_CLIENT_ID = "Afflo6fi72lrSEjUs6h1N3wSoqQc4v3yqTZxQNLcnoNVoii9mhuW8--PYyVlQ270fWG7bhC-dHgHlnsK";
+}
